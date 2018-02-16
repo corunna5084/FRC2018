0498db023ea34c1f6db62e06234d1b80aba7c1c1
FridgeBot <corunna5084@gmail.com>
2018-02-16 18:03:01 -0500

    Add files via upload

---
+++ Gyro Mecanum/src/org/usfirst/frc/team5084/robot/Robot.java
@@ -0,0 +1,81 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team5084.robot;
+
+import edu.wpi.first.wpilibj.AnalogGyro;
+import com.analog.adis16448.*;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.*;
+
+/**
+ * This is a sample program that uses mecanum drive with a gyro sensor to
+ * maintian rotation vectorsin relation to the starting orientation of the robot
+ * (field-oriented controls).
+ */
+public class Robot extends IterativeRobot {
+	// gyro calibration constant, may need to be adjusted;
+	// gyro value of 360 is set to correspond to one full revolution
+	private static final double kVoltsPerDegreePerSecond = 0.0128;
+
+	private static final int kFrontLeftChannel = 0;
+	private static final int kRearLeftChannel = 1;
+	private static final int kFrontRightChannel = 2;
+	private static final int kRearRightChannel = 3;
+	private static final int kGyroPort = 0;
+	private static final int kJoystickPort = 0;
+
+	private MecanumDrive m_robotDrive;
+	private AnalogGyro m_gyro = new AnalogGyro(kGyroPort);
+	private Joystick m_joystick = new Joystick(kJoystickPort);
+	
+	public static final ADIS16448_IMU imu = new ADIS16448_IMU();
+	
+	@Override
+	public void robotInit() {
+		Spark frontLeft = new Spark(kFrontLeftChannel);
+		Spark rearLeft = new Spark(kRearLeftChannel);
+		Spark frontRight = new Spark(kFrontRightChannel);
+		Spark rearRight = new Spark(kRearRightChannel);
+
+		// Invert the left side motors.
+		// You may need to change or remove this to match your robot.
+		frontLeft.setInverted(true);
+		rearLeft.setInverted(true);
+
+		m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+
+		m_gyro.setSensitivity(kVoltsPerDegreePerSecond);
+	}
+
+	/**
+	 * Mecanum drive is used with the gyro angle as an input.
+	 */
+	@Override
+	public void teleopPeriodic() {
+		m_robotDrive.driveCartesian(m_joystick.getX(), m_joystick.getY(),
+				m_joystick.getZ(), m_gyro.getAngle());
+		
+		SmartDashboard.putNumber("Gyro-X", imu.getAngleX());
+	    SmartDashboard.putNumber("Gyro-Y", imu.getAngleY());
+	    SmartDashboard.putNumber("Gyro-Z", imu.getAngleZ());
+	    
+	    SmartDashboard.putNumber("Accel-X", imu.getAccelX());
+	    SmartDashboard.putNumber("Accel-Y", imu.getAccelY());
+	    SmartDashboard.putNumber("Accel-Z", imu.getAccelZ());
+	    
+	    SmartDashboard.putNumber("Pitch", imu.getPitch());
+	    SmartDashboard.putNumber("Roll", imu.getRoll());
+	    SmartDashboard.putNumber("Yaw", imu.getYaw());
+	    
+	    SmartDashboard.putNumber("Pressure: ", imu.getBarometricPressure());
+	    SmartDashboard.putNumber("Temperature: ", imu.getTemperature());
+	}
+}
